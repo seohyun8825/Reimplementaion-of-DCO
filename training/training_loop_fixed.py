6a156c09665f0e869986f00d5df3c974430bfde4
unknown <smart8825@naver.com>
2024-08-27 20:19:42 +0900

    fixed algorithm not debugged yet

--- training/training_loop_fixed.py
+++ training/training_loop_fixed.py
@@ -3,6 +3,7 @@ import gc
 import logging
 import copy
 import progressbar
+from collections import deque
 from training.utils import compute_text_embeddings, compute_time_ids
 from training.checkpoint import save_checkpoint
 from training.loss import compute_loss
@@ -16,8 +17,9 @@ def train_loop(args, accelerator, models, noise_scheduler, optimizer, lr_schedul
     embedding_handler = models.get("embedding_handler")
 
     refer_model = copy.deepcopy(unet)
+    refer_model.eval()  # Set the reference model to evaluation mode
     for param in refer_model.parameters():
-        param.requires_grad = False
+        param.requires_grad = False  # Freeze the reference model
 
     global_step, first_epoch = 0, 0
 
@@ -34,6 +36,8 @@ def train_loop(args, accelerator, models, noise_scheduler, optimizer, lr_schedul
     progress_bar = progressbar.ProgressBar(max_value=args.max_train_steps, widgets=widgets)
     progress_bar.start()
 
+    model_outputs = deque(maxlen=50)  # Deque to store model outputs for averaging
+
     for epoch in range(first_epoch, args.num_train_epochs):
         unet.train()
         if args.train_text_encoder:
@@ -42,88 +46,75 @@ def train_loop(args, accelerator, models, noise_scheduler, optimizer, lr_schedul
 
         for step, batch in enumerate(train_dataloader):
             with accelerator.accumulate(unet):
-                try:
-                    prompts = batch["prompts"]
-                    pixel_values = batch["pixel_values"].to(accelerator.device, dtype=vae.dtype)
-                    latents = vae.encode(pixel_values).latent_dist.sample() * vae.config.scaling_factor
-                    noise = torch.randn_like(latents)
-                    timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (latents.shape[0],)).long().to(latents.device)
-                    noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
+                prompts = batch["prompts"]
+                pixel_values = batch["pixel_values"].to(accelerator.device, dtype=vae.dtype)
 
-                    prompt_embeds, pooled_prompt_embeds = compute_text_embeddings(prompts, text_encoders, tokenizers, accelerator)
-                    added_cond_kwargs = {"text_embeds": pooled_prompt_embeds, "time_ids": time_ids}
-                    model_output = unet(noisy_latents, timesteps, prompt_embeds, added_cond_kwargs=added_cond_kwargs).sample
+                # Encode original pixel values to latents
+                latents = vae.encode(pixel_values).latent_dist.sample() * vae.config.scaling_factor
+                noise = torch.randn_like(latents)
+                timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (latents.shape[0],)).long().to(latents.device)
+                noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
 
-                    #model_output 을 똑같이 vae를 한번 더 태워서 latent를 얻어내야함
+                prompt_embeds, pooled_prompt_embeds = compute_text_embeddings(prompts, text_encoders, tokenizers, accelerator)
+                added_cond_kwargs = {"text_embeds": pooled_prompt_embeds, "time_ids": time_ids}
 
-                    # for loosing latent
-                    # 똑같이 이미지를 픽셀벨류로 바꿔야함
-                    losing_pixel_values = model_output  #몇스텝이전의 model output 을 가져와야할것 같긴함. deque
-                    losing_latents = vae.encode(losing_pixel_values).latent_dist.sample() * vae.config.scaling_factor
-                    losing_noise = torch.randn_like(losing_latents)
-                    losing_timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (losing_latents.shape[0],)).long().to(losing_latents.device)
-                    losing_noisy_latents = noise_scheduler.add_noise(losing_latents, noise, timesteps)
+                model_output = unet(noisy_latents, timesteps, prompt_embeds, added_cond_kwargs=added_cond_kwargs).sample
+                model_outputs.append(model_output.detach())  # Append output to deque
 
+                if step % 50 == 0 and len(model_outputs) > 0:
+                    average_model_output = torch.mean(torch.stack(list(model_outputs)), dim=0)
+                    model_outputs.clear()  # Clear the deque after calculating the average
 
-                    with torch.no_grad():
-                        losing_refer_output = refer_model(losing_noisy_latents, timesteps, prompt_embeds, added_cond_kwargs = added_cond_kwargs).sample
-                
-                    losing_model_output = unet(losing_noisy_latents, timesteps, prompt_embeds, added_cond_kwargs = added_cond_kwargs).sample
+                with torch.no_grad():
+                    refer_output = refer_model(noisy_latents, timesteps, prompt_embeds, added_cond_kwargs).sample
 
-                    with torch.no_grad():
-                        refer_output = refer_model(noisy_latents, timesteps, prompt_embeds, added_cond_kwargs=added_cond_kwargs).sample
+                # Use the latest average model output as losing_pixel_values
+                if average_model_output is not None:
+                    losing_latents = vae.encode(average_model_output).latent_dist.sample() * vae.config.scaling_factor
+                else:
+                    losing_latents = vae.encode(model_output.detach()).latent_dist.sample() * vae.config.scaling_factor
 
-                    target_win = noise
-                    target_lose = losing_noise
+                losing_noise = torch.randn_like(losing_latents)
+                losing_timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (losing_latents.shape[0],)).long().to(losing_latents.device)
+                losing_noisy_latents = noise_scheduler.add_noise(losing_latents, losing_noise, losing_timesteps)
 
-                    loss_model_win = compute_loss(model_output, target_win, noise_scheduler, timesteps, latents)
+                with torch.no_grad():
+                    losing_refer_output = refer_model(losing_noisy_latents, losing_timesteps, prompt_embeds, added_cond_kwargs).sample
 
+                target_win = noise
+                target_lose = losing_noise
 
-                    loss_model_lose = compute_loss(losing_model_output, target_lose, noise_scheduler, losing_timesteps, losing_latents)
-                    ##losing noise timestep 바꿔야함
+                loss_model_win = compute_loss(model_output, target_win, noise_scheduler, timesteps, latents)
+                loss_model_lose = compute_loss(unet(losing_noisy_latents, losing_timesteps, prompt_embeds, added_cond_kwargs).sample, target_lose, noise_scheduler, losing_timesteps, losing_latents)
 
-                    if args.dcoloss > 0.0:
-                        loss_refer_win = compute_loss(refer_output, target_win, noise_scheduler, timesteps, latents)
+                if args.dcoloss > 0.0:
+                    loss_refer_win = compute_loss(refer_output, target_win, noise_scheduler, timesteps, latents)
+                    loss_refer_lose = compute_loss(losing_refer_output, target_lose, noise_scheduler, losing_timesteps, losing_latents)
+                    
+                    diff_win = loss_model_win - loss_refer_win
+                    diff_lose = loss_model_lose - loss_refer_lose
+                    
+                    diff = diff_win - diff_lose
+                    loss = -1 * torch.nn.LogSigmoid()(args.dcoloss * diff)
+                else:
+                    loss = loss_model_win
 
-                        loss_refer_lose = compute_loss(losing_refer_output, target_lose, noise_scheduler, losing_timesteps, losing_latents)
-                        diff_win = loss_model_win - loss_refer_win
+                accelerator.backward(loss)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
 
-                        diff_lose = loss_model_lose - loss_refer_lose
-                        print("diff_lose : ")
+                if accelerator.sync_gradients:
+                    global_step += 1
+                    progress_bar.update(global_step, loss=loss.item())
+                    logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
+                    accelerator.log(logs, step=global_step)
 
-                        diff = diff_win - diff_lose
+                    if global_step % args.checkpoint_save == 0:
+                        save_checkpoint(accelerator, args, unet, text_encoder_one, text_encoder_two, embedding_handler, global_step)
 
-                        inside_term = -1 * args.dcoloss * diff
-
-                        loss = -1 * torch.nn.LogSigmoid()(inside_term)
-                    else:
-                        loss = loss_model_win
-
-                    accelerator.backward(loss)
-
-                    if accelerator.sync_gradients:
-                        torch.nn.utils.clip_grad_norm_(unet.parameters(), 1.0)
-
-                    optimizer.step()
-                    lr_scheduler.step()
-                    optimizer.zero_grad()
-
-                    if accelerator.sync_gradients:
-                        global_step += 1
-                        progress_bar.update(global_step, loss=loss.item())
-
-                        logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
-                        accelerator.log(logs, step=global_step)
-
-                        if global_step % args.checkpoint_save == 0:
-                            save_checkpoint(accelerator, args, unet, text_encoder_one, text_encoder_two, embedding_handler, global_step)
-
-                    if global_step >= args.max_train_steps:
-                        break
-
-                except Exception as e:
-                    logger.error(f"Error at epoch {epoch}, step {step}: {e}")
-                    raise
+                if global_step >= args.max_train_steps:
+                    break
 
         gc.collect()
         torch.cuda.empty_cache()
